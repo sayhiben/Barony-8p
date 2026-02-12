ea5ad633d9c97af1794585c944a5f374fb8c6e26
WALL OF JUSTICE <->
2026-02-13 08:14:35 +1100

    * infected branch bow now spores impact now counts as magicstaff vs casted spell

--- src/magic/actmagic.cpp
+++ src/magic/actmagic.cpp
@@ -10612,7 +10612,7 @@ Entity* floorMagicCreateRoots(real_t x, real_t y, Entity* caster, int damage, in
 	return spellTimer;
 }
 
-void floorMagicCreateSpores(Entity* spawnOnEntity, real_t x, real_t y, Entity* caster, int damage, int spellID)
+void floorMagicCreateSpores(Entity* spawnOnEntity, real_t x, real_t y, Entity* caster, int damage, int spellID, bool magicstaff)
 {
 	if ( multiplayer == CLIENT )
 	{
@@ -10688,6 +10688,7 @@ void floorMagicCreateSpores(Entity* spawnOnEntity, real_t x, real_t y, Entity* c
 	spellTimer->particleTimerVariable1 = damage;
 	spellTimer->particleTimerVariable2 = spellID;
 	spellTimer->particleTimerVariable4 = 0;
+	spellTimer->actmagicCastByMagicstaff = magicstaff ? 1 : 0;
 
 	auto& timerEffects = particleTimerEffects[spellTimer->getUID()];
 
@@ -13736,6 +13737,7 @@ void actParticleTimer(Entity* my)
 								fx->sizex = 8;
 								fx->sizey = 8;
 								fx->parent = my->getUID();
+								fx->actmagicCastByMagicstaff = my->actmagicCastByMagicstaff;
 								floorMagicParticleSetUID(*fx, true);
 							}
 						}
