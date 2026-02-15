3e5eed3478a245aee86f5b66f66d67b816887150
WALL OF JUSTICE <->
2026-02-15 20:49:45 +1100

    * NPC with rapier no ATK penalty * offhand items with no AC dont add to AC effectiveness * debug doors check key locks * map hash update * remove double sfx deface spell

--- src/entity.cpp
+++ src/entity.cpp
@@ -8150,7 +8150,8 @@ real_t Entity::getACEffectiveness(Entity* my, Stat* myStats, bool isPlayer, Enti
 		return 1.0;
 	}
 
-	if ( myStats->defending || (myStats->parrying && myStats->weapon && itemCategory(myStats->weapon) == WEAPON) )
+	if ( (myStats->defending && !(myStats->shield && !Item::doesItemProvideBeatitudeAC(myStats->shield->type)))
+		|| (myStats->parrying && myStats->weapon && itemCategory(myStats->weapon) == WEAPON) )
 	{
 		return 1.0;
 	}
@@ -8284,7 +8285,7 @@ Sint32 Entity::getAttack(Entity* my, Stat* myStats, bool isPlayer, int chargeMod
 	if ( !shapeshifted && myStats->weapon && myStats->weapon->type == RAPIER )
 	{
 		int atk = statGetDEX(myStats, my);
-		if ( chargeModifier >= 0 && chargeModifier < Stat::getMaxAttackCharge(myStats) )
+		if ( chargeModifier >= 0 && chargeModifier < Stat::getMaxAttackCharge(myStats) && my && my->behavior == &actPlayer )
 		{
 			atk /= 2;
 		}
