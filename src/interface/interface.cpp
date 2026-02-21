0c8839f345cbc49baa0e40bc32ac8a02f4b59c33
SheridanR <sheridan.rathbun@gmail.com>
2026-02-20 19:09:29 -0800

    fix uninitialized pointer in interface.cpp
    Signed-off-by: SheridanR <sheridan.rathbun@gmail.com>

--- src/interface/interface.cpp
+++ src/interface/interface.cpp
@@ -9231,7 +9231,7 @@ void GenericGUIMenu::alchemyCookCombination()
 		if ( alchemyGUI.torchCount.count >= 0 )
 		{
 			std::vector<Item*> torches;
-			Item* torchesEquipped;
+			Item* torchesEquipped{};
 			for ( node_t* invnode = stats[gui_player]->inventory.first; invnode != NULL; invnode = invnode->next )
 			{
 				Item* item = (Item*)invnode->element;
